cc8a527abd506ece32a7e01aaf21cc4a6b365c32
brian <joynerbc@g.cofc.edu>
2021-09-02 12:46:03 -0700

    Finished Homework One

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name: <Brianna Joyner>
+hello.py
+
+Problem: This code solves the problem of printing phrases
+through following the rules of quotations and parentheses.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work
+"""
+
+def main():
+    print("hello, world!")
+
+main()
