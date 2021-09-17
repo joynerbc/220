34c5ee948358dca5914972ff63653c26433fbfc5
brian <joynerbc@g.cofc.edu>
2021-09-16 20:35:58 -0700

    Finished Homework Two

--- assignments/hw2/interest.py
+++ assignments/hw2/interest.py
@@ -11,18 +11,18 @@ I certify that this assignment is my own work, but I discussed it with tutors at
 def main():
     annual_apr = eval(input("enter the annual interest rate: "))
     annual_apr /= 100
-    billing_cycle = eval(input("enter the number of billing days: "))
+    billing_cycle = eval(input("enter the number of billing days in the billing cycle: "))
     previous_balance = eval(input("enter the previous net balance: "))
     payment = eval(input("enter the payment amount: "))
     cycle_day = eval(input("enter the day of the billing cycle when the payment was made: "))
     step_one = previous_balance * billing_cycle
-    days_before_end_cycle = billing_cycle - cycle_day
-    step_two = payment * days_before_end_cycle
+    step_two = payment * (billing_cycle - cycle_day)
     step_three = step_one - step_two
-    step_four = step_three / billing_cycle
+    balance_daily = step_three / billing_cycle
     monthly_interest_rate = annual_apr / 12
-    monthly_interest_charge = step_four * monthly_interest_rate
-    print(round(monthly_interest_charge, 2))
+    monthly_interest_cost = balance_daily * monthly_interest_rate
+    monthly_interest_cost = round(monthly_interest_cost, 2)
+    print(monthly_interest_cost)
 
 
 if __name__ == '__main__':
